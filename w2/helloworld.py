4bcbecbd9bd9573255bb8fab10b362ec5b6e7571
LuzRosas <marilurs@hotmail.com>
2020-10-17 04:39:58 +0000

    Semana 3 de curso python

---
+++ w2/helloworld.py
@@ -0,0 +1,6 @@
+# Autor: Luz Maria Rosas Salcedo
+# Universidad Veracruzana
+# 8 de octubre de 2020
+print ("Hello world  !")
+print ("Hola mundo")
+print ("Hola python")
---
+++ w2/variables.py
@@ -0,0 +1,21 @@
+# variables
+# Author : Adolfo Centeno
+# Oct 2020
+
+year = 2020
+sentence = "My name is mrosas"
+sarah, bob, mike = 16, 20, 25
+name, age = "mary", 24
+mary = fer = 24
+PI = 3.1416
+e=2.71
+
+# display values
+print "year = ", year
+print sentence
+print "\n sarah = ", sarah, "\n bob =", bob, "\n mike =", mike
+print "\n\t name =", name
+print "age =", age
+print mary, fer
+print PI
+print e
